a8d009969b9d79b5e38d7bef331dc2f9f9bf5a90
Owen Gitau <owengitau02@gmail.com>
2024-05-17 13:17:16 +0300

    changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,34 +1,14 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
-export default function Navbar({ onSearch }) {
-  const [searchTerm, setSearchTerm] = useState("");
-
-  const handleSearch = () => {
-    onSearch(searchTerm);
-  };
-
+export default function Navbar() {
   return (
     <div>
       <nav className="flex justify-between items-center mb-6 bg-green-600 p-4 rounded-md shadow-md">
         <NavLink to="/records" className="text-white text-lg font-bold">
           <h1>GreenBelt EHRS</h1>
         </NavLink>
-
-        <div className="flex items-center">
-          <input
-            type="text"
-            placeholder="Search by name"
-            className="border border-gray-300 rounded-md p-1 mr-2"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <button
-            className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium text-white ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-white bg-green-500 hover:bg-green-700 h-9 rounded-md px-3 mr-16"
-            onClick={handleSearch}
-          >
-            Search
-          </button>
+        <div className="flex items-center space-x-4">
           <NavLink
             className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium text-white ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-white bg-green-500 hover:bg-green-700 h-9 rounded-md px-3"
             to="/create"
